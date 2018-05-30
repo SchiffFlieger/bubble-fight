885d5147726b0486a946837b919be0aa74ca9fbf
Karsten Koehler <karsten.koehler95@gmail.com>
2018-05-31 00:53:29 +0200

    shoot ring

---
+++ Assets/Scripts/Ring.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ring : MonoBehaviour
+{
+
+    public int speed;
+
+private bool active;
+private Vector3 initialPos;
+    // Use this for initialization
+    void Start()
+    {
+        active = false;
+        initialPos = this.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            active = true;
+            Rigidbody2D body = GetComponent<Rigidbody2D>();
+            Vector2 direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - this.transform.position;
+            body.velocity = direction.normalized * speed;
+        }
+
+        if (!active) 
+        {
+            this.transform.position = initialPos;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        print("colliding");
+    }
+}
