5bd31b03a862c2923471e8c4f5c99e4e11d6c261
Karsten Koehler <karsten.koehler95@gmail.com>
2018-05-30 17:46:45 +0200

    draw shooting line

---
+++ Assets/Scripts/ShootingLine.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootingLine : MonoBehaviour
+{
+
+    public GameObject ring;
+
+    void Start()
+    {
+        LineRenderer line = GetComponent<LineRenderer>();
+
+        line.positionCount = 2;
+        line.startWidth = 0.15f;
+        line.endWidth = 0.15f;
+        line.useWorldSpace = true;
+
+        line.SetPosition(0, ring.transform.position);
+    }
+
+    void Update()
+    {
+        LineRenderer line = GetComponent<LineRenderer>();
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        line.SetPosition(1, mousePos);
+    }
+}
