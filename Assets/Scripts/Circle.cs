9cbed3770281b2cbad0b4a710d8e580536594797
SchiffFlieger <karsten.koehler95@gmail.com>
2018-06-01 17:59:31 +0200

    destroy circle on hit

---
+++ Assets/Scripts/Circle.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Circle : MonoBehaviour {
+
+	public int hits;
+
+	private TextMesh text;
+
+	void Start() 
+	{
+		this.text = this.transform.Find("Number").GetComponent<TextMesh>();
+		UpdateCircle();	
+	}
+
+	void OnCollisionEnter2D(Collision2D collision)
+	{
+		if (collision.gameObject.CompareTag("Ring"))
+		{
+			this.hits--;
+			UpdateCircle();
+		}
+	}
+
+	private void UpdateCircle()
+	{
+		if (this.hits <= 0)
+		{
+			GameObject.Destroy(this.gameObject);
+		}
+		this.text.text = "" + this.hits;
+	}
+}
