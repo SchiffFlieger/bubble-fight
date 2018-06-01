846638322a5a754eee60005b84b20abf62eb1d9d
SchiffFlieger <karsten.koehler95@gmail.com>
2018-06-01 23:03:02 +0200

    add circle manager to control game states

---
+++ Assets/Scripts/CircleManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircleManager : MonoBehaviour {
+
+	enum State {
+		Idle, RingSpawning, Shooting, Refreshing
+	}
+
+	public int shootsTillNextRow;
+
+	private State state;
+	private int shootsSinceLastRow;
+
+	void Start () {
+		this.state = State.Idle;		
+		this.shootsSinceLastRow = 0;
+	}
+	
+	void Update () {
+		switch(state) {
+			case State.Idle:
+				UpdateIdle();
+				break;
+			case State.RingSpawning:
+				UpdateRingSpawning();
+				break;
+			case State.Shooting:
+				UpdateShooting();
+				break;
+			case State.Refreshing:
+				UpdateRefreshing();
+				break;
+			default:
+				print("something strange happened");
+		}
+	}
+
+	void UpdateIdle() 
+	{
+		// wait for mouse click
+		// change state to ring spawning
+	}
+
+	void UpdateRingSpawning()
+	{
+		// spawn a new ring every xx frames and apply velocity
+		// if all rings are spawned, change state to shooting
+	}
+	
+	void UpdateShooting()
+	{
+		// move rings
+		// check if there are rings left
+		// change state to refreshing if no rings left
+	}
+
+	void UpdateRefreshing()
+	{
+		// spawn new row if necessary
+		// change state to idle
+	}
+}
