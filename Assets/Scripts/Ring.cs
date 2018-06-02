1e6564acef30405c3d8f742d39f10707809fa689
SchiffFlieger <karsten.koehler95@gmail.com>
2018-06-02 13:02:29 +0200

    destroy ring on collision with bottom

--- Assets/Scripts/Ring.cs
+++ Assets/Scripts/Ring.cs
@@ -4,5 +4,16 @@ using UnityEngine;
 
 public class Ring : MonoBehaviour
 {
+    void OnDestroy() 
+    {
 
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Bottom"))
+        {
+            Destroy(this.gameObject);
+        }
+    }
 }
