2fe56c1ce68a4076262033f265bc0d588161963e
Karsten Koehler <karsten.koehler95@gmail.com>
2018-08-08 05:18:51 +0200

    display ring count and ring damage in hud

---
+++ Assets/Scripts/HudManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HudManager : MonoBehaviour {
+
+	public Text ringCountText;
+	public Text ringDamageText;
+	public Text scoreText;
+
+	private CircleManager circleManager;
+	private int dummyScore;
+
+
+	void Start () {
+		this.circleManager = GameObject.FindObjectOfType<CircleManager>();		
+		this.dummyScore = 0;
+	}
+	
+	void Update () {
+		dummyScore++;
+		this.ringCountText.text = "Rings: " + circleManager.ringsToShoot.ToString();
+		this.ringDamageText.text = "Damage: " + circleManager.ringDamage.ToString();
+		this.scoreText.text = "Score: " + dummyScore.ToString();
+	}
+}
