3a0d734747084c9ac95fbf4efcd2142d6166c646
Karsten Koehler <karsten.koehler95@gmail.com>
2018-08-13 16:24:48 +0200

    add color to circles

--- Assets/Scripts/Circle.cs
+++ Assets/Scripts/Circle.cs
@@ -2,41 +2,52 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Circle : MonoBehaviour {
+public class Circle : MonoBehaviour
+{
+    public NumberDisplay display;
 
-	public int maxHits;
-	public NumberDisplay display;
-
-	private int hits;
+    private int hitsLeft;
     private ScoreManager scoreManager;
-	private UpgradeManager upgradeManager;
+    private UpgradeManager upgradeManager;
 
-	void Start() 
-	{
-		this.hits = Random.Range(1, maxHits);
-		
+    private SpriteRenderer spriteRenderer;
+
+
+    void Start()
+    {
+        this.hitsLeft = Random.Range(1, 7);
+
         this.scoreManager = GameObject.FindObjectOfType<ScoreManager>();
-		this.upgradeManager = GameObject.FindObjectOfType<UpgradeManager>();
-		display.SetNumber(hits);
-	}
+        this.upgradeManager = GameObject.FindObjectOfType<UpgradeManager>();
+        display.SetNumber(hitsLeft);
+        this.spriteRenderer = GetComponent<SpriteRenderer>();
+    }
 
-	void OnCollisionEnter2D(Collision2D collision)
-	{
-		if (collision.gameObject.CompareTag("Ring"))
-		{
-			this.hits -= this.upgradeManager.RingDamage();
-			UpdateCircle();
-		}
-	}
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ring"))
+        {
+            this.hitsLeft -= this.upgradeManager.RingDamage();
+            UpdateCircle();
+        }
+    }
 
-	private void UpdateCircle()
-	{
-		if (this.hits <= 0)
-		{
-			scoreManager.AddScore(this.maxHits);
-			GameObject.Destroy(this.gameObject);
-			GameObject.Destroy(this.display.gameObject);
-		}
-		this.display.SetNumber(this.hits);
-	}
+    private void UpdateCircle()
+    {
+        if (this.hitsLeft <= 0)
+        {
+            scoreManager.AddScore(7);
+            GameObject.Destroy(this.gameObject);
+            GameObject.Destroy(this.display.gameObject);
+        }
+        this.display.SetNumber(this.hitsLeft);
+    }
+
+    void Update()
+    {
+        float red = Mathf.Atan(this.hitsLeft / 10.0f) - Mathf.PI / 6.0f;
+        float green = 100.0f / this.hitsLeft;
+
+        this.spriteRenderer.color = new Color(red, green, 0, 1.0f);
+    }
 }
