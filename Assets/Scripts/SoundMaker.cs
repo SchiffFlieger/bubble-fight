dda9b26dcd9cc698d028058c64877f5ea1ef6ba5
Karsten Koehler <karsten.koehler95@gmail.com>
2018-08-29 19:36:56 +0200

    add menu button sounds

---
+++ Assets/Scripts/SoundMaker.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundMaker : MonoBehaviour
+{
+    public AudioClip hover;
+    public AudioClip click;
+
+    public void PlayHoverSound()
+    {
+        AudioSource.PlayClipAtPoint(hover, transform.position);
+    }
+    public void PlayClickSound()
+    {
+        AudioSource.PlayClipAtPoint(click, transform.position);
+    }
+}
